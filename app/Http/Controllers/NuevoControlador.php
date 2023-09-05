7faca9a7c8c4f446c43edee4ad62c9f19836a376
Pablo-935 <pabloferreyra935@gmail.com>
2023-09-05 17:41:47 -0300

    Primer Commit

---
+++ app/Http/Controllers/NuevoControlador.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NuevoControlador extends Controller
+{
+    public function miFuncion(){
+        return view('mi-vista');
+    }
+}
